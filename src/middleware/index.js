33863508ab82d32c1f269536e85973052aa16509
Steffan <steffanhalv@gmail.com>
2022-07-22 12:23:44 +0200

    Added bucket

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -4,8 +4,11 @@ import registerTypes from '../register-types.js'
 import services from '../services/index.js'
 import channels from '../channels.js'
 import authentication from '../authentication.js'
+import mongodb from 'mongodb'
 
 export default app => {
+  let db = this.app.get('mongooseClient').connection.client.db()
+  app.set('bucket', new mongodb.GridFSBucket(db, { bucketName: 'uploads' }))
   if (!app.io) app.configure(socketio(io => adapter(io, app)))
   app.configure(services)
   app.configure(authentication)
