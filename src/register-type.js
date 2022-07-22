141cd37b2e446342353ade605f3b337e8389716b
Steffan <steffanhalv@gmail.com>
2022-07-22 16:26:09 +0200

    Set mime type automatically

--- src/register-type.js
+++ src/register-type.js
@@ -48,7 +48,10 @@ export default async (app, type) => {
 
     app.use('/types/' + type.slug, new Type(options, app), (req, res, next) => {
       res.set('Cache-Control', 'no-store')
-      if (res?.data?.buffer && res?.data['content-type']) {
+      if (res?.data?.buffer && res?.data?.filename) {
+        res.type(res.data.filename.split('.').pop())
+        res.send(res.data.buffer)
+      } else if (res?.data?.buffer && res?.data['content-type']) {
         res.set('Content-Type', res.data['content-type'])
         res.send(res.data.buffer)
       } else {
