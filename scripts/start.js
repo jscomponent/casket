d5f8640bc6aa4516b95cad8b8b52d2b61dc2580c
Steffan <steffanhalv@gmail.com>
2022-08-09 17:27:11 +0200

    Removed watcher

--- scripts/start.js
+++ scripts/start.js
@@ -3,6 +3,6 @@
 import { execSync } from 'child_process'
 import name from './ensure-name.js'
 
-execSync(`NODE_OPTIONS=--experimental-vm-modules npm run build; pm2 start src/index.js -n ${name} -f --watch; pm2 save`)
+execSync(`NODE_OPTIONS=--experimental-vm-modules npm run build; pm2 start src/index.js -n ${name} -f; pm2 save`)
 
 console.log(`Instance ${name} has started`)
