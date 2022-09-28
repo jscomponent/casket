1d3caf522201f661b4f231893dfb058d818a23ca
Steffan <steffanhalv@gmail.com>
2022-09-28 15:56:32 +0200

    Addded facebook and github strategies

--- src/authentication.js
+++ src/authentication.js
@@ -1,6 +1,7 @@
 import { AuthenticationService, AuthenticationBaseStrategy, JWTStrategy } from '@feathersjs/authentication'
 import { LocalStrategy } from '@feathersjs/authentication-local'
 import { oauth, OAuthStrategy } from '@feathersjs/authentication-oauth'
+import axios from 'axios'
 
 export default app => {
   const authentication = new AuthenticationService(app)
@@ -9,6 +10,8 @@ export default app => {
   authentication.register('local', new LocalStrategy())
   authentication.register('anonymous', new AnonymousStrategy())
   authentication.register('google', new GoogleStrategy())
+  authentication.register('facebook', new FacebookStrategy())
+  authentication.register('github', new GithubStrategy())
 
   app.set('oauth', oauth)
 
@@ -22,15 +25,55 @@ class AnonymousStrategy extends AuthenticationBaseStrategy {
   }
 }
 
+class GithubStrategy extends OAuthStrategy {
+  async getProfile (authResult) {
+    const accessToken = authResult.access_token
+    const { data } = await axios.get('https://api.github.com/user', {
+      headers: {
+        authorization: `Bearer ${accessToken}`
+      }
+    })
+    return data
+  }
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile)
+    return {
+      ...baseData,
+      name:  profile.name,
+      email: profile.email
+    }
+  }
+}
+
 class GoogleStrategy extends OAuthStrategy {
   async getEntityData(profile) {
-    // this will set 'googleId'
     const base = await super.getEntityData(profile)
-
-    // this will grab the picture and email address of the Google profile
     return {
       ...base,
       profilePicture: profile.picture,
+      email: profile.email
+    }
+  }
+}
+
+class FacebookStrategy extends OAuthStrategy {
+  async getProfile (authResult) {
+    const accessToken = authResult.access_token
+    const { data } = await axios.get('https://graph.facebook.com/me', {
+      headers: {
+        authorization: `Bearer ${accessToken}`
+      },
+      params: {
+        fields: 'id,name,email,picture'
+      }
+    })
+    return data
+  }
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile)
+    return {
+      ...baseData,
+      name:  profile.name,
       email: profile.email
     }
   }
