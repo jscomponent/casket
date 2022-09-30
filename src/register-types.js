e10e25dec57e4c61c7a093132df31aa6f277ccaf
Steffan <steffanhalv@gmail.com>
2022-09-30 13:40:29 +0200

    Updated types limit

--- src/register-types.js
+++ src/register-types.js
@@ -3,7 +3,7 @@ import register from './register-type.js'
 export default async app => {
   let mongoClient = app.get('mongooseClient').connection.client
   let collection = mongoClient.db().collection('types/any')
-  let types = await collection.find().limit(100)
+  let types = await collection.find().limit(1000)
   let arr = await types.toArray()
   for await (const type of arr) await register(app, type)
 }
