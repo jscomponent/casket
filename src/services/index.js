51393b0b32e56b3212dcaaf856f086f99472fa77
Steffan <steffanhalv@gmail.com>
2022-12-20 09:15:39 +0100

    Force change git

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,10 @@
 import users from './users/users.service.js'
 import types from './types/types.service.js'
 export default async app => {
+
   if (app.lookup('/users')) await app.unuse('/users')
   if (app.lookup('/types/any')) await app.unuse('/types/any')
   app.configure(users)
   app.configure(types)
+  
 }
