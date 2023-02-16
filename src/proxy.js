cb2c2e7869b3f444e00ac8e0dc112201b2bbd61a
Steffan <steffanhalv@gmail.com>
2023-02-16 13:22:14 +0100

    Made ssl work again

--- src/proxy.js
+++ src/proxy.js
@@ -115,8 +115,8 @@ export default (app) => {
             res.end()
         }
         if (target.secure) {
-            //res.writeHead(301, {Location: `https://${req.headers.host}${req.url}`})
-            //res.end()
+            res.writeHead(301, {Location: `https://${req.headers.host}${req.url}`})
+            res.end()
         }
         proxy.web(req, res, { target }, e => {
             proxy.web(req, res, { target: { host: 'localhost', port: 8001 } }, err => {
