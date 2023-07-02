066324370eddfe8a2e78c61a6c3811642190e8ac
Steffan <steffanhalv@gmail.com>
2023-07-03 01:19:37 +0200

    Add safelist

--- admin/assets/html-ab2bc3a1.js
+++ admin/assets/html-ab2bc3a1.js
@@ -1,4 +1,4 @@
-import{m as p}from"./Type-5aa31045.js";import"./index-dbb4f506.js";import"./moment-fbc5633a.js";/*!-----------------------------------------------------------------------------
+import{m as p}from"./Type-975686f6.js";import"./index-b82fdb5d.js";import"./moment-fbc5633a.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.36.1(6c56744c3419458f0dd48864520b759d1a3a1ca8)
  * Released under the MIT license
--- admin/assets/javascript-f9c4686a.js
+++ admin/assets/javascript-f9c4686a.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-17a855ad.js";import"./Type-5aa31045.js";import"./index-dbb4f506.js";import"./moment-fbc5633a.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-2b18a843.js";import"./Type-975686f6.js";import"./index-b82fdb5d.js";import"./moment-fbc5633a.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.36.1(6c56744c3419458f0dd48864520b759d1a3a1ca8)
  * Released under the MIT license
